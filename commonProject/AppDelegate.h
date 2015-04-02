e6c9140d93e821c4777bab7b10e1a8ac6df4fa55
lerpo <563253419@qq.com>
2015-04-02 11:24:02 +0800

    Initial Commit

---
+++ commonProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  commonProject
+//
+//  Created by JEREI on 15-4-2.
+//  Copyright (c) 2015年 com.xu.tableview. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ commonProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  commonProject
+//
+//  Created by JEREI on 15-4-2.
+//  Copyright (c) 2015年 com.xu.tableview. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
