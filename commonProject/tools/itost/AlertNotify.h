54f473f09327df98c881acdd128619d6cfc40835
lerpo <563253419@qq.com>
2015-04-14 07:57:34 +0800

    4.14

--- commonProject/tools/itost/AlertNotify.h
+++ commonProject/tools/itost/AlertNotify.h
@@ -9,9 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface AlertNotify : UIView
-+(void)ShowSuccess:(NSString *)message;
-+(void)ShowFaild:(NSString *)message;
-+(void)ShowTitle:(NSString *)title withDetail:(NSString *)detail;
-+(void)ShowLoading:(NSString *)message forView:(UIView *)view;
-+(void)Stoploading;
++(void)ShowSuccess:(NSString *)message;   //成功消息提示
++(void)ShowFaild:(NSString *)message;     //失败消息提示
++(void)ShowTitle:(NSString *)title withDetail:(NSString *)detail;  //详细提醒信息提示
++(void)ShowLoading:(NSString *)message forView:(UIView *)view;     //加载进度信息提示
++(void)Stoploading;  //停止加载
 @end
